b7e6f4dbf1913620e87464ed897b9227ad4f8cff
TheLoweDown256 <200691122+TheLoweDown256@users.noreply.github.com>
2026-08-11 20:23:23 -0400

    Add settings

--- OrangeText/OrangeTextPatches.cs
+++ OrangeText/OrangeTextPatches.cs
@@ -14,6 +14,7 @@ namespace OrangeText
         [HarmonyPatch(typeof(TextTranslation), nameof(TextTranslation._Translate))]
         public static void TextTranslation__Translate_Postfix(string key, TextTranslation __instance, ref string __result)
         {
+            if (!OrangeText.Instance.ModHelper.Config.GetSettingsValue<bool>("Affect general text")) return;
             __result = OrangeText.Instance.ProcessText(__result);
         }
 
@@ -22,15 +23,17 @@ namespace OrangeText
         [HarmonyPatch(typeof(TextTranslation), nameof(TextTranslation._Translate_ShipLog))]
         public static void TextTranslation__Translate_ShipLog_Postfix(string key, TextTranslation __instance, ref string __result)
         {
+            if (!OrangeText.Instance.ModHelper.Config.GetSettingsValue<bool>("Affect ship log text")) return;
             __result = OrangeText.Instance.ProcessText(__result);
         }
 
 
-        /*[HarmonyPostfix]
+        [HarmonyPostfix]
         [HarmonyPatch(typeof(TextTranslation), nameof(TextTranslation._Translate_UI))]
         public static void TextTranslation__Translate_UI_Postfix(int key, TextTranslation __instance, ref string __result)
         {
+            if (!OrangeText.Instance.ModHelper.Config.GetSettingsValue<bool>("Affect UI text")) return;
             __result = OrangeText.Instance.ProcessText(__result);
-        }*/
+        }
     }
 }
